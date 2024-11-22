335fb2a3da20cf4ae2a06ea0e4cdfc0958a9b94a
한대현 <choco5958@naver.com>
2024-11-22 17:33:21 +0900

    CICD 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Test Get started by editing <code>src/app/page.tsx</code>.
+            CICD Test Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
