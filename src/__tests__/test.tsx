50a5df26f4a1e1b1d2ef246a8de512d190b8a1c0
한대현 <choco5958@naver.com>
2024-11-22 23:52:40 +0900

    임시테스트코드

--- src/__tests__/test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Home from '@/app/page';
-
-test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn/i);
-  expect(linkElement).toBeInTheDocument();
-});
