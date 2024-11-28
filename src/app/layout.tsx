7f7dbfe11efb3c9c1688f66b2f2fb7768114a69b
한대현 <choco5958@naver.com>
2024-11-28 13:33:50 +0900

    eslint 에러 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import Header from '@/components/common/header/header';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import Header from '@/components/common/header/header';
 
 export default function RootLayout({
   children,
--- src/app/utility/imageConverter/layout.tsx
+++ src/app/utility/imageConverter/layout.tsx
@@ -3,5 +3,5 @@ export default function UtilityImageConverterLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return { children };
 }
