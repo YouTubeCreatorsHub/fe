179feb26a37598077bfb733392afb10bc89c0c2c
한대현 <choco5958@naver.com>
2024-12-05 01:37:31 +0900

    test

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -2,14 +2,19 @@ module.exports = {
   apps: [
     {
       name: 'next-app',
-      script: 'npm',
+      script: 'node_modules/next/dist/bin/next',
       args: 'start',
       instances: '1',
-      exec_mode: 'cluster',
+      exec_mode: 'fork',
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
       },
+      watch: false,
+      max_memory_restart: '1G',
+      error_file: 'logs/err.log',
+      out_file: 'logs/out.log',
+      time: true,
     },
   ],
 };
