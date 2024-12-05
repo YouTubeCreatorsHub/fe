9b1647a891d03be9d72a1f4f6e2523c76c31e20d
한대현 <choco5958@naver.com>
2024-12-05 21:49:33 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import YoutubeTrendingList from '@/components/domain/main/youtubeTrendingList';
 export default function MainPage() {
   return (
     <>
-      <h1>test메인 페이지입니다.</h1>
+      <h1>메인 페이지입니다.</h1>
       <YoutubeTrendingList />
     </>
   );
