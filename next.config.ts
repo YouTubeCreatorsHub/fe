392aafbe3785a9131ae5436c5655e1dceacd044a
lim3873 <lim3873@naver.com>
2024-12-06 04:46:39 +0900

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,21 @@
 import type { NextConfig } from 'next';
+const path = require('path');
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, 'src'),
+      },
+      // 대소문자 구분 강제
+      enforceExtension: false,
+      enforceModuleExtension: false,
+      fullySpecified: false,
+    };
+    return config;
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
