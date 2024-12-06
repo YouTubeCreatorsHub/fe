a9419413a1f4be8f02fa90a906642134e3276210
lim3873 <lim3873@naver.com>
2024-12-06 09:10:20 +0900

    update package.json

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,16 @@
 import type { NextConfig } from 'next';
-import type { Configuration as WebpackConfig } from 'webpack';
 const path = require('path');
 
 const nextConfig: NextConfig = {
-  webpack: (config: WebpackConfig) => {
+  webpack: (config) => {
     config.resolve = {
       ...config.resolve,
       alias: {
         '@': path.resolve(__dirname, 'src')
       }
     };
-    
     return config;
   }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
