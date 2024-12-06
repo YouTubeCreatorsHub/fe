5f6b26b78ad2575fd1c46a1b6df1976afd285b85
한대현 <choco5958@naver.com>
2024-12-06 17:29:04 +0900

    설정파일

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import nextPlugin from '@next/eslint-plugin-next';
+import typescriptParser from '@typescript-eslint/parser';
+
+module.exports = [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+];
