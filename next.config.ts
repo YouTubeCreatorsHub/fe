2236e81de35d231e8d7f3d0dda3e849f02bc40ea
한대현 <choco5958@naver.com>
2024-12-06 17:58:01 +0900

    설정

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from 'next';
-const path = require('path');
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-      },
-    };
-    return config;
-  },
+
 };
 
 export default nextConfig;
