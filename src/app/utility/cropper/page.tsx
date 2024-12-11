b7bf6a20019813dcc8f17af05fe165ed883a0a9a
한대현 <choco5958@naver.com>
2024-12-11 18:13:01 +0900

    오류체크

--- src/app/utility/cropper/page.tsx
+++ src/app/utility/cropper/page.tsx
@@ -3,7 +3,7 @@ import Cropper from '@/components/domain/utility/Cropper';
 export default function UtilityCropperPage() {
   return (
     <>
-      <h1>Cropper 페이지입니다</h1>
+      <h1>Cropper 페이지입니다1</h1>
       <Cropper />
     </>
   );
